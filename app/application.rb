820b23fd67cda803c1e5366ff0f464e6eeea32b4
Ayana Bando <ayanaflax@yahoo.com>
2018-09-20 18:14:52 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,10 +3,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
  
-    num_1 = Kernel.rand(1..20)
-    num_2 = Kernel.rand(1..20)
-    num_3 = Kernel.rand(1..20)
- 
+    time = Time.new
     resp.write "#{num_1}\n"
     resp.write "#{num_2}\n"
     resp.write "#{num_3}\n"
