238d8b04c9f098a763c65e931e04e4721a97b403
Ayana Bando <ayanaflax@yahoo.com>
2018-09-20 18:17:54 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,11 +1,10 @@
-require 'pry'
 class Application
  
   def call(env)
     resp = Rack::Response.new
  
     time = Time.new(2018, 9, 20, 12, 00, 00)
-    binding.pry
+   
 
     if num_1==num_2 && num_2==num_3
       resp.write "You Win"
